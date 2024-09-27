56c2948fc7ac9f2db90d2262af40782df0158464
devGusta2 <gustavo.souza2979@gmail.com>
2024-09-27 08:30:52 -0300

    Aula 05 - Stack

---
+++ Aula 05 - Stack/app.js
@@ -0,0 +1,12 @@
+ //lifo - ultimo que entrou é o que eu tenho que remover
+
+ //tamanho fixo - heap é uma memoria limitada quando o tamanho é excedido tem o stack overflow
+
+ // Manipulações da pilha, push, size, top, empty
+/*
+ Push - adiciona elemento
+ Size - número total de elementos da pilha
+ Top - Elemento do topo da linha
+ Pop - Remove e retorna o elemento do topo
+ Empty - Verifica se é vazia (true ou false)
+*/
