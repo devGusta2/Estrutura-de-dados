cc6b02c92d9ac18331e639bee2c3f5975b26482e
devgusta2 <gustavo.souza2979@gmail.com>
2024-11-01 08:23:23 -0300

    Listar

---
+++ Listas/index.js
@@ -0,0 +1,28 @@
+/* em teoria nao tem limite de espaço
+na prática a lista estoura causando lsit overflow;
+
+lista simples encadeada - adiciona os itens em um unico sentido linear;
+
+
+lista duplamente encadeada - sentido unico  e permie a navegação nas duas direções
+esquerda para direita ou direita para esquerda;
+
+lista circular - quando o limite é atingido o proximo elemento adicionado
+vai substituir o primeiro elemento da lista;
+------------------------------------------------------------------
+Operações - Adicionar ou (ADD) - adiciona elementos
+
+Remover - DELETE - remove o elemento
+
+Obter Get - Pega o elemento
+
+Tamanho SIZE - retorna o tamanho da lista
+
+Empyty - verifica se esta vazia
+------------------------------------------------------------------
+
+
+
+// dentro do java é AarrayList<tipo_dado> nome array =  new ArrayList<>();
+
+*/
