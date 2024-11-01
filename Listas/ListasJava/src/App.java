971e1bbbe72f5d16c45ecd2724ced45178d3fa58
devgusta2 <gustavo.souza2979@gmail.com>
2024-11-01 08:30:11 -0300

    Listas java

--- Listas/ListasJava/src/App.java
+++ Listas/ListasJava/src/App.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        // declarando a lista
+        ArrayList<String> frutas = new ArrayList<>();
+
+
+        // adicionar elementoS : ADD
+
+        frutas.add("Banana");
+        frutas.add("Laranja");
+        frutas.add("Melancia");
+        frutas.add("Uva");
+        System.out.println(frutas);
     }
 }
