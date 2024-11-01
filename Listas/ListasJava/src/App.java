3e76df0d4331816cf00d48ad72721956ffd764a2
devgusta2 <gustavo.souza2979@gmail.com>
2024-11-01 09:53:32 -0300

    Lista encadeada

--- Listas/ListasJava/src/App.java
+++ Listas/ListasJava/src/App.java
@@ -1,34 +1,60 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 public class App {
     public static void main(String[] args) throws Exception {
-        // declarando a lista
-        ArrayList<String> frutas = new ArrayList<>();
+        lista_encadeada();
+    }
+    public static void lista_encadeada(){
+        // lista encadeada
+
+        LinkedList<String> carros =  new LinkedList<>();
+        carros.add("Corsa");
+        carros.add("Gold");
+        carros.add("Hb10");
+        carros.add("Onix");
+
+        carros.add("Kwid");
+        System.out.println(carros.toString());
+
+        // adicionando no primeiro espaço
+        carros.addFirst("Fiat");
+        // adicionando no ultimo espaço
+        carros.addLast("Honda");
+    }
+
+    public static void lista_simples(){
+         // declarando a lista
+         ArrayList<String> frutas = new ArrayList<>();
 
 
-        // adicionar elementoS : ADD
-
-        frutas.add("Banana");
-        frutas.add("Laranja");
-        frutas.add("Melancia");
-        frutas.add("Uva");
-        
-
-        // remover
-        frutas.remove(1);
-
-        // consultar elementos de maneira direta
-
-        System.out.println(frutas.get(0));
-
-        // consultar o tamanho da lista
-        System.out.println("O tamanho da lista é:"+frutas.size());
-
-        // verificar se a lista está vazia
-        System.out.println("A lista está vazia ?");
-        if(frutas.isEmpty()){
-            System.out.println("Está vazia");
-        }else{
-            System.out.println("Não está vazia");
-        }
+         // adicionar elementoS : ADD
+ 
+         frutas.add("Banana");
+         frutas.add("Laranja");
+         frutas.add("Melancia");
+         frutas.add("Uva");
+         
+ 
+         // remover
+         frutas.remove(1);
+ 
+         // consultar elementos de maneira direta
+ 
+         System.out.println(frutas.get(0));
+ 
+         // consultar o tamanho da lista
+         System.out.println("O tamanho da lista é:"+frutas.size());
+ 
+         // verificar se a lista está vazia
+         System.out.println("A lista está vazia ?");
+         if(frutas.isEmpty()){
+             System.out.println("Está vazia");
+         }else{
+             System.out.println("Não está vazia");
+         }
+ 
+         for(String item:frutas){
+             System.out.println(item);
+         }
     }
 }
