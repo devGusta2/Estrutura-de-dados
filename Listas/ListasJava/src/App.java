bcbbba7ea822098574b511d8256d8d69e25eb1f4
devgusta2 <gustavo.souza2979@gmail.com>
2024-11-01 08:39:23 -0300

    Funções de lista

--- Listas/ListasJava/src/App.java
+++ Listas/ListasJava/src/App.java
@@ -11,6 +11,24 @@ public class App {
         frutas.add("Laranja");
         frutas.add("Melancia");
         frutas.add("Uva");
-        System.out.println(frutas);
+        
+
+        // remover
+        frutas.remove(1);
+
+        // consultar elementos de maneira direta
+
+        System.out.println(frutas.get(0));
+
+        // consultar o tamanho da lista
+        System.out.println("O tamanho da lista é:"+frutas.size());
+
+        // verificar se a lista está vazia
+        System.out.println("A lista está vazia ?");
+        if(frutas.isEmpty()){
+            System.out.println("Está vazia");
+        }else{
+            System.out.println("Não está vazia");
+        }
     }
 }
