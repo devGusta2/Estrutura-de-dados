d6c6cce35adf70310bded24ee95b309248d57fae
devgusta2 <gustavo.souza2979@gmail.com>
2024-11-29 08:11:10 -0300

    Filas - JAVA

---
+++ Filas Java/src/App.java
@@ -0,0 +1,25 @@
+
+//FIFO (FIRST IN - FIRST OUT) - FILA estrutura de dados 29/11/2024
+// fila é limitada - overflow
+// Fila circular - os elementos sao usados de maneira circular onde o ultimo elmento esta agarrado ao primeiro elemento
+// Fila circular nao permite dar o overflow, vai subistituindo os dados
+
+// Filas de prioridade - dependendo da prioridade ele pode ser colocado no inicio da fila (enqueue)
+
+// remover - desenfileiramento pelo primeiro
+
+
+// Fila dupla
+
+/*
+    --enqueue - enfileira
+    -- size - retornar o tamanho
+    --dequeue - tira da fila
+    --empty - verifica se é vazio
+*/
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
